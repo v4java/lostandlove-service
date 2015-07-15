5f6931268538067d7d54fb27f7b7a6da46ea47fc
vincent4java <qq37740048@gmail.com>
2015-07-15 14:20:19 +0800

    工作流

--- src/main/java/com/v4java/lal/dao/JobsUserDao.java
+++ src/main/java/com/v4java/lal/dao/JobsUserDao.java
@@ -3,12 +3,8 @@ package com.v4java.lal.dao;
 import java.util.List;
 
 import com.v4java.lal.pojo.JobsUser;
-import com.v4java.lal.view.admin.WorkFlowVO;
 
 public interface JobsUserDao {
-
-	
-
 	
 	void batchInsertJobsUser(List<JobsUser> jobsUsers) throws Exception;
 	
