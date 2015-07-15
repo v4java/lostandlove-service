f81fd6f5ed5fd5d8a0514a82a64c5bfc649921fb
vincent4java <qq37740048@gmail.com>
2015-07-15 15:00:27 +0800

    状态

---
+++ src/main/java/com/v4java/lal/common/FlowConst.java
@@ -0,0 +1,13 @@
+package com.v4java.lal.common;
+
+/**
+ * 
+ * @author vincent
+ *
+ */
+public class FlowConst {
+	//同意
+	public static final int AGREE_TRUE = 0;
+	//拒绝
+	public static final int AGREE_FALSE = 1;
+}
