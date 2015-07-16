43cf4119884f3a15add80f61cd3996294fd728bf
vincent4java <qq37740048@gmail.com>
2015-07-16 14:01:44 +0800

    update

---
+++ src/main/java/com/v4java/lal/service/impl/WorkFlowModelServiceImpl.java
@@ -0,0 +1,26 @@
+package com.v4java.lal.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.v4java.lal.dao.WorkFlowModelDao;
+import com.v4java.lal.pojo.WorkFlowModel;
+import com.v4java.lal.service.IWorkFlowModelService;
+
+
+@Service("workFlowModelService")
+public class WorkFlowModelServiceImpl implements IWorkFlowModelService{
+
+	@Autowired
+	private WorkFlowModelDao workFlowModelDao ;
+	@Override
+	public WorkFlowModel findWorkFlowModelById(Integer id) throws Exception {
+		return workFlowModelDao.findWorkFlowModelById(id);
+	}
+
+	@Override
+	public void insertWorkFlowModel(WorkFlowModel workFlowModel)throws Exception {
+		workFlowModelDao.insertWorkFlowModel(workFlowModel);
+	}
+
+}
